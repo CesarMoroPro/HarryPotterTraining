55f2fa05d98c1e7f2026034c9ca5cca8b6bdc0d2
César MORO <cesar.moro.dev@gmail.com>
2022-11-27 12:12:15 +0100

    vanillaAvadaVsExpel : avancement dans la fonction interne avec 2 parties : une partie commune et une partie avec du code individuel selon condition. //+// Dans la partie individuelle, avancement sur le visuel des sortilèges

--- integration/assets/js/utils/vanillaFunctions/vanillaAvadaVsExpelliarmus.js
+++ integration/assets/js/utils/vanillaFunctions/vanillaAvadaVsExpelliarmus.js
@@ -1,69 +1,89 @@
-
 //! STEP 1 = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
 //^ Je déclare de la fonction "vanillaAvadaExperlliarmus()"
 export function vanillaAvadaVsExpelliarmus() {
 
-    //! STEP 2 = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
-    //^ Logique programmatique de la fonction
-
+    //! STEP 2 = = = = = = LOGIQUE PROGRAMMATIQUE = = = = = = = = = = = = = = = = = = = = = = = =
+    //! STEP 2.A = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+    //^ Déclaration des variables
     /**
      * Il s'agit des deux divs d'ID "avada-spell" et "expelliarmus-spell"
      * ? Rappel : via jQuery, on récupère un objet !!!
      */
-    let avadaSpellJQueryObjet                    = $('#avada-spell');
-    let avadaSpell                               = avadaSpellJQueryObjet[0];
-    let expelliarmusSpellJQueryObjet             = $('#expelliarmus-spell');
-    let expelliarmusSpell                        = expelliarmusSpellJQueryObjet[0];
-
+    let avadaSpellJQueryObject                    = $('#avada-spell');
+    let avadaSpell                                = avadaSpellJQueryObject[0];
+    let expelliarmusSpellJQueryObject             = $('#expelliarmus-spell');
+    let expelliarmusSpell                         = expelliarmusSpellJQueryObject[0];
+ 
+    //! STEP 2.B = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+    //^ Fonction interne pour donner du mouvement aux sortilèges
     /**
      ** Je crée une fonction générique "spellsAction()" pour les mouvements des sortilèges.
      * Cette fonction prend un seul paramètre, qui aura pour argument le sort concerné.
      */
-    function spellsAction(paramSpell) {
+    function spellsAction(paramSpell, paramSpellJQueryObject) {
 
         /**
-         ** Dans cette première partie, je prépare tout ce qui sera commun aux deux sortilèges :
-         * ... - Durée de transition
-         * ... - animate.css > pulse
+         *= STEP 2.B.1 : Dans cette première partie, je prépare tout ce qui sera commun aux deux sortilèges :
+         * ... - Transition : Durée + Ease
+         * ... - Width des div sortilèges
          * ... - faire "vaguer" les sorts
          * ... - former une boule au point de rencontre
          * ... - ajouter (opacité + flou) en mode aléatoire
          */
 
-        // Durée de transition :
-        paramSpell.style.transitionDuration          = '2s';
-
-        // Animate.css > pulse
-        // 1. Ajouter la class "pulse" aux éléments
-        // 2. Englober cette class "pulse" dans un setInterval afin de répéter le mouvement
-        setInterval(() => {
-            paramSpell.classList.toggle('');
-        }, 1000);
+         paramSpellJQueryObject.css({
+            'transition'    : '3s ease-in',
+            'width'         : '100%'
+        })
 
         
-
-
-        
-
 
         /**
-         ** Dans cette seconde partie, j'inclus une condition selon le sort concerné :
+         *= STEP 2.B.2 : Dans cette seconde partie, j'inclus une condition selon le sort concerné :
          * ... - si le sort est 'avadaSpell', alors bloc d'instructions;
-         * ... - si le sort est 'expelliarmusSpell', alors bloc d'instructions.
+         * ... - sinon si le sort est 'expelliarmusSpell', alors bloc d'instructions.
          * 
-         * ...Attention, faire varier les épaisseurs (hauteurs) des sorts aléatoirement
+         * ...Attention : faire varier les épaisseurs (height) des sorts
          * ...doit être défini ici et non dans la partie commune de "spellsActions()"
-         * ...afin de générer des épaisseurs différentes aux deux sorts ! Visuellement plus beau et naturel.
+         * ...afin de générer des épaisseurs différentes aux deux sorts ! Effet visuel plus naturel et plus beau.
+         * ...Pour faire varier les épaisseurs de manière aléatoire, on englobe le tout dans une fonction générant un nombre aléatoire
          * ...Pour ce faire, on utilise la fonction "randomNumberSpellsHeight()" dans les utils > vanillaFunctions
          */
         if(paramSpell === avadaSpell) {
-            paramSpell.style.width            = '100%';
-        }
-        else if(paramSpell === expelliarmusSpell) {
-            paramSpell.style.width     = '100%';
+            /**
+             ** Ici, je fais appel à la fonction setInterval
+             * ...pour modifier l'épaisseur du sort toutes les xxx millisecondes spécifiées.
+             */
+            setInterval(() => {
+                /**
+                 ** Je génère un nombre aléatoire entre [0, 1[
+                 * Je multiplie ce nombre par 20 pour avoir des nombres entre 0 et 19,999...
+                 * J'exclus les valeurs proches de 0 en arrondissant au dessus avec .ceil()
+                 * J'exclus les petites valeurs en ajoutant 3 par exemple
+                 */
+                let randomNumber1 = Math.ceil(Math.random()*20)+3;
+
+                //* Je définit la nouvelle épaisseur de l'élément 
+                paramSpellJQueryObject.css({
+                    'height': randomNumber1
+                });
+            }, 500);            
+        } else if(paramSpell === expelliarmusSpell) {
+            setInterval(() => {
+                /**
+                 ** Je génère un autre nombre aléatoire selon les mêmes critères
+                *...mais une seconde fois afin d'avoir des nombres différents sur les deux sorts
+                */
+                let randomNumber2 = Math.ceil(Math.random()*20)+3;
+
+                //* Je définit la nouvelle épaisseur de l'élément 
+                paramSpellJQueryObject.css({
+                    'height': randomNumber2
+                });
+            }, 500);
         }
     };
 
-    spellsAction(avadaSpell);
-    spellsAction(expelliarmusSpell)
+    spellsAction(avadaSpell, avadaSpellJQueryObject);
+    spellsAction(expelliarmusSpell, expelliarmusSpellJQueryObject);
 }
