4f1ca38c2a4910d2caca542e63bb66d121b29262
César MORO <cesar.moro.dev@gmail.com>
2022-11-27 12:25:10 +0100

    Refactorisation du code dans vanillaAvadaVsExpelliarmus

--- integration/assets/js/utils/vanillaFunctions/vanillaAvadaVsExpelliarmus.js
+++ integration/assets/js/utils/vanillaFunctions/vanillaAvadaVsExpelliarmus.js
@@ -42,43 +42,40 @@ export function vanillaAvadaVsExpelliarmus() {
          *= STEP 2.B.2 : Dans cette seconde partie, j'inclus une condition selon le sort concerné :
          * ... - si le sort est 'avadaSpell', alors bloc d'instructions;
          * ... - sinon si le sort est 'expelliarmusSpell', alors bloc d'instructions.
-         * 
-         * ...Attention : faire varier les épaisseurs (height) des sorts
-         * ...doit être défini ici et non dans la partie commune de "spellsActions()"
+         ** L'épaisseurs (height) des sorts doit être définie ici et non dans la partie commune de "spellsActions()"
          * ...afin de générer des épaisseurs différentes aux deux sorts ! Effet visuel plus naturel et plus beau.
-         * ...Pour faire varier les épaisseurs de manière aléatoire, on englobe le tout dans une fonction générant un nombre aléatoire
-         * ...Pour ce faire, on utilise la fonction "randomNumberSpellsHeight()" dans les utils > vanillaFunctions
          */
-        if(paramSpell === avadaSpell) {
+         
+        //* Pour faire varier les épaisseurs de manière aléatoire, je crée une fonction générant un nombre aléatoire qui sera retourné
+        function randomNumberSpellsHeight() {
+
             /**
-             ** Ici, je fais appel à la fonction setInterval
-             * ...pour modifier l'épaisseur du sort toutes les xxx millisecondes spécifiées.
+             * Je multiplie ce nombre par 20 pour avoir des nombres entre 0 et 19,999...
+             *...J'exclus les valeurs proches de 0 en arrondissant au dessus avec .ceil()...
+             *...J'exclus les petites valeurs en ajoutant 3 par exemple
              */
-            setInterval(() => {
-                /**
-                 ** Je génère un nombre aléatoire entre [0, 1[
-                 * Je multiplie ce nombre par 20 pour avoir des nombres entre 0 et 19,999...
-                 * J'exclus les valeurs proches de 0 en arrondissant au dessus avec .ceil()
-                 * J'exclus les petites valeurs en ajoutant 3 par exemple
-                 */
-                let randomNumber1 = Math.ceil(Math.random()*20)+3;
+            
+            let randomNumber = Math.ceil(Math.random()*20)+3;
+            return randomNumber;
+        }
 
-                //* Je définit la nouvelle épaisseur de l'élément 
+        //* Puis j'utilise cette fonction dès lors que je veux générer une épaisseur aléatoire
+        if(paramSpell === avadaSpell) {
+
+            setInterval(() => {
+                //* Je définit la nouvelle épaisseur de l'élément grâce à la fonction retournant mon nombre aléatoire
                 paramSpellJQueryObject.css({
-                    'height': randomNumber1
+                    'height': randomNumberSpellsHeight()
                 });
-            }, 500);            
+            }, 500);
+            
         } else if(paramSpell === expelliarmusSpell) {
-            setInterval(() => {
-                /**
-                 ** Je génère un autre nombre aléatoire selon les mêmes critères
-                *...mais une seconde fois afin d'avoir des nombres différents sur les deux sorts
-                */
-                let randomNumber2 = Math.ceil(Math.random()*20)+3;
 
-                //* Je définit la nouvelle épaisseur de l'élément 
+            setInterval(() => {
+
+                //* Idem que dans la condition "if"
                 paramSpellJQueryObject.css({
-                    'height': randomNumber2
+                    'height': randomNumberSpellsHeight()
                 });
             }, 500);
         }
